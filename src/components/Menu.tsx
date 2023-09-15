a635043932aa0a1ae892067967b07f7f9392fb3e
Juan Cardona <juanscr24@hotmail.com>
2023-09-15 12:02:00 -0500

    First commit

---
+++ src/components/Menu.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+      <Image src='/open.png' alt='foto 1' width={20} height={20}/>
+    </div>
+  )
+}
+
+export default Menu
